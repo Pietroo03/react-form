4ec01947509c1ec28b42f6f3c1f11b4b63fef889
Pietroo03 <pietro.ponte.03@gmail.com>
2024-11-18 14:30:45 +0100

    add form

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -6,6 +6,22 @@ export default function AppMain() {
 
             <div className="container">
                 <h2>Articles</h2>
+
+                <form>
+                    <div className="mb-3">
+                        <label htmlFor="title" className="form-label">Title</label>
+
+                        <div class="input-group mb-3">
+                            <input type="text"
+                                class="form-control"
+                                placeholder="Aggiungi Titolo"
+                                aria-label="Recipient's username"
+                                aria-describedby="button-addon2" />
+
+                            <button class="btn btn-primary" type="submit" id="button-addon2">Button</button>
+                        </div>
+                    </div>
+                </form>
             </div>
 
         </main>
