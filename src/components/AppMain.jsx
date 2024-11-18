d9059a3cb5aad8608e8826a1e5326abae3046a39
Pietroo03 <pietro.ponte.03@gmail.com>
2024-11-18 16:12:37 +0100

    add input for Author

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -5,16 +5,23 @@ export default function AppMain() {
 
     const [articles, setArticle] = useState(articlesList)
     const [newArticle, setNewArticle] = useState('')
+    const [newAuthor, setNewAuthor] = useState('')
 
     function addArticle(e) {
         e.preventDefault(e)
 
+        const newArticleData = {
+            title: newArticle,
+            author: newAuthor
+        }
+
         setArticle([
             ...articles,
-            newArticle
+            newArticleData
         ])
 
         setNewArticle('')
+        setNewAuthor('')
     }
 
     function handleRemove(e) {
@@ -47,7 +54,24 @@ export default function AppMain() {
                                 value={newArticle}
                                 onChange={e => setNewArticle(e.target.value)} />
 
-                            <button className="btn btn-primary" type="submit" id="button-addon2">Button</button>
+                            <button className="btn btn-primary" type="submit" id="button-addon2">Aggiungi Titolo</button>
+                        </div>
+
+                    </div>
+
+                    <div className="mb-3">
+                        <label htmlFor="author" className="form-label">Autore</label>
+
+                        <div className="input-group mb-3">
+                            <input type="text"
+                                className="form-control"
+                                placeholder="Aggiungi Autore"
+                                aria-label="Recipient's username"
+                                aria-describedby="button-addon2"
+                                value={newAuthor}
+                                onChange={e => setNewAuthor(e.target.value)} />
+
+                            <button className="btn btn-primary" type="submit" id="button-addon2">Aggiungi Autore</button>
                         </div>
 
                     </div>
@@ -58,7 +82,10 @@ export default function AppMain() {
                 <ul className="list-group">
                     {articles.map((article, index) =>
                         <li key={index} className="list-group-item d-flex justify-content-between align-items-center">
-                            {article}
+                            <div>
+                                <div>Titolo: <strong>{article.title}</strong></div>
+                                <div>Autore: <strong>{article.author}</strong></div>
+                            </div>
 
                             <button onClick={handleRemove} data-index={index} className="btn btn-danger">Remove</button>
                         </li>
