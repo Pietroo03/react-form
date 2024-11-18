d109e3ea8249851bf75276191c34b63c623b132a
Pietroo03 <pietro.ponte.03@gmail.com>
2024-11-18 14:49:36 +0100

    add useStates
    add function to add an article

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,4 +1,22 @@
+import { useState } from "react"
+const articlesList = []
+
 export default function AppMain() {
+
+    const [articles, setArticle] = useState(articlesList)
+    const [newArticle, setNewArticle] = useState('')
+
+    function addArticle(e) {
+        e.preventDefault(e)
+
+        setArticle([
+            ...articles,
+            newArticle
+        ])
+        console.log(articles);
+
+
+    }
 
     return (
 
@@ -7,21 +25,36 @@ export default function AppMain() {
             <div className="container">
                 <h2>Articles</h2>
 
-                <form>
+                <form onSubmit={addArticle}>
                     <div className="mb-3">
                         <label htmlFor="title" className="form-label">Title</label>
 
-                        <div class="input-group mb-3">
+                        <div className="input-group mb-3">
                             <input type="text"
-                                class="form-control"
+                                className="form-control"
                                 placeholder="Aggiungi Titolo"
                                 aria-label="Recipient's username"
-                                aria-describedby="button-addon2" />
+                                aria-describedby="button-addon2"
+                                value={newArticle}
+                                onChange={e => setNewArticle(e.target.value)} />
 
-                            <button class="btn btn-primary" type="submit" id="button-addon2">Button</button>
+                            <button className="btn btn-primary" type="submit" id="button-addon2">Button</button>
                         </div>
+
                     </div>
+
                 </form>
+
+                <h2>Articles List</h2>
+                <ul className="list-group">
+                    {articles.map((article, index) =>
+                        <li key={index} className="list-group-item">
+                            {article}
+                        </li>
+
+                    )}
+                </ul>
+
             </div>
 
         </main>
