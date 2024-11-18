429aa9f2af238bd365b99962a73b6bc70242749e
Pietroo03 <pietro.ponte.03@gmail.com>
2024-11-18 14:22:23 +0100

    add folder components
    create AppHeader component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
+import AppHeader from './components/AppHeader'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+
     <>
 
+      <AppHeader />
+
     </>
+
   )
 }
 
